081f0336c3e646ca4533e71fa9bdf35c13c9cbfe
dcry2306 <147155497+dcry2306@users.noreply.github.com>
2024-03-31 00:22:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author_email='kilaz2306@gmail.com',
     name='dcrypr1',
     version='0.0.1',
-    description='A simple package for https://app.tea.xyz/. Example pr1 - https://github.com/dcry2306/pr2.git and pr2 - https://github.com/dcry2306/pr2.git',
+    description='A simple package for https://app.tea.xyz/. Example pr1 - https://github.com/dcry2306/pr1.git and pr2 - https://github.com/dcry2306/pr2.git',
     url='https://github.com/dcry2306/pr.git',
     project_urls={
         'Homepage': 'https://github.com/dcry2306/pr.git',
